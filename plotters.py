987b5a085878db940e6f6ad00ef0f1997b794f63
Donald Keeney <donald.keeney@gmail.com>
2019-09-06 13:05:37 -0400

    multis

--- plotters.py
+++ plotters.py
@@ -26,3 +26,19 @@ def plot_roc_curve(fpr, tpr, label='ROC'):
     plt.legend()# Add axis labels and grid
     plt.xlabel('False Positive Rate', fontsize=11)
     plt.ylabel('Recall/TPR', fontsize=11)
+
+
+def plot_digits(instances, images_per_row=10, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size,size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap = mpl.cm.binary, **options)
+    plt.axis("off")
